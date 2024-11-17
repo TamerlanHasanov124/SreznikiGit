ed3a9f3039f344d88c9e7bb32adad933b71b24f7
TamerlanHasanov124 <hasanov.tamerlan.96@gmail.com>
2024-11-17 16:34:53 +0400

    ByRamazan

--- Source.cpp
+++ Source.cpp
@@ -1,17 +1,50 @@
 #include<iostream>
-#include<forward_list>
+#include<forward_list> // odnosvazanniy
+#include<list> // dvuxsvazanniy spisok
 using namespace std;
 
-int main() {
-	forward_list<string> srezniki;
-	srezniki.push_front("Ramazan");
-	srezniki.push_front("Ramin");
-	srezniki.push_front("Rauf");
+class User {
+private:
+	string name;
+	string surname;
+	int age;
+public:
+	User(string name, string surname, int age) : name(name), surname(surname), age(age) {}
 
-	for (auto i : srezniki)
-	{
-		cout << i << endl;
+	string GetName() {
+		return name;
 	}
+
+	string GetSurname() {
+		return surname;
+	}
+
+	int GetAge()
+	{
+		return age;
+	}
+};
+
+void AddStudent(forward_list<User>& srezniki, string name, string surname, int age) {
+	srezniki.push_front(User(name,surname,age));
+}
+
+void PrintList(forward_list<User>& srezniki) {
+	for (auto item : srezniki) {
+		cout << item.GetName() << " " << item.GetSurname() << endl;
+		
+	}
+}
+
+
+int main() {
+	forward_list<User> srezniki;
+
+	AddStudent(srezniki,"Tamerlan","Hasanov",28);
+	AddStudent(srezniki, "Fazil", "Mammadov", 22);
+	AddStudent(srezniki, "Ilkin", "Agayev", 28);
+
+	PrintList(srezniki);
 
 
 	return 0;
